572c6c5ce395753e004123b24a36bc01d0e78e7a
bauti <bautiquere@gmail.com>
2020-07-10 13:14:29 -0300

    base get

--- app/services/BaseService.js
+++ app/services/BaseService.js
@@ -1,3 +1,11 @@
-const baseUrl = 'http://localhost:3000/';
+import axios from 'axios';
 
-module.exports = {baseUrl};
+export const get = (path, params, body) => {
+    const url = `http://localhost:3000/${path}?${params}`;
+
+    axios.get(url, body)
+            .then( function(r){ return r })
+            .catch(function(error) { return error });
+}
+
+module.exports = {get};
