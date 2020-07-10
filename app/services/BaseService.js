c25437baf3f14537c143e4f49b6cef6a37ab2f34
bauti <bautiquere@gmail.com>
2020-07-10 17:05:07 -0300

    add more base service options

--- app/services/BaseService.js
+++ app/services/BaseService.js
@@ -2,10 +2,9 @@ import axios from 'axios';
 
 const baseUrl = "http://localhost:3000/";
 
-export function get(callback, path, params, body) {
+function generateUrl(path, params) {
     var url = `${baseUrl}`;
 
-   
     if (!!path) {
         url+= path
     }
@@ -13,10 +12,26 @@ export function get(callback, path, params, body) {
     if (!!params) {
         url += params
     }
+    return url;
+}
 
-
-
+export function get(callback, path, params, body) {
+    const url = generateUrl(path, params);
     axios.get(url, body)
-            .then( function(r) { callback(r) })
-            .catch(function(error) { callback(error) });
+            .then( function(r) { callback(r, undefined) })
+            .catch(function(error) { callback(undefined, error) });
+}
+
+export function post(callback, path, params, body) {
+    const url = generateUrl(path, params);
+    axios.post(url, body)
+            .then( function(r) { callback(r, undefined) })
+            .catch(function(error) { callback(undefined, error) });
+}
+
+export function put(callback, path, params, body) {
+    const url = generateUrl(path, params);
+    axios.put(url, body)
+            .then( function(r) { callback(r, undefined) })
+            .catch(function(error) { callback(undefined, error) });
 }
