3817beb572321f5115f72098d18286576bfb06b0
bauti <bautiquere@gmail.com>
2020-07-16 18:13:35 -0300

    alert in browser

--- app/components/AuthAlert.js
+++ app/components/AuthAlert.js
@@ -1,16 +1,16 @@
 
-import {Alert} from 'react-native';
+import {Alert, Platform} from 'react-native';
 
 const AuthAlert = {
 
     showAlert(error)  {
         const errorMessage = !!error && !!error.response && !!error.response.data ? error.response.data : "Hubo un problema en la conexion";
-        Alert.alert(
-            "Error",
-            errorMessage,[
-              { text: "OK"}],
-            { cancelable: false }
-          );
+
+        if (Platform.OS === 'web') {
+          alert(errorMessage)
+       } else {
+           Alert.alert("Error", errorMessage, { text: "OK"})
+       }
     }
 }
 
