b81b7348338f81d2e2abe6514abbd0cc98e19580
Franco De Angelis <franco.de.angelis@outlook.com>
2020-07-16 18:21:59 -0300

    remove innecesary comments

--- Constantes.js
+++ Constantes.js
@@ -1,8 +1,5 @@
 import { Dimensions } from 'react-native';
 
-// export default Constantes = {
-//     
-// }
 const Constants = {
     MAX_WIDTH: Dimensions.get("screen").width,
     MAX_HEIGHT: Dimensions.get("screen").height,
